0d8b34bf0ea8bbda79028a3aa704542bcc53814f
Alessandro Zuccarello <alessandro@MacBook-Air-di-Alessandro.local>
2025-09-17 02:49:05 +0200

    Collegamento con backend, le chiamate funzionano in quanto health funziona ma sembra non connettersi al DB

---
+++ TAW/frontend/src/app/core/flight.models.ts
@@ -0,0 +1,36 @@
+export type Cabin = 'economy' | 'premium' | 'business' | 'first';
+
+export interface FlightSearchParams {
+  from: string;
+  to: string;
+  departDate: string;
+  returnDate: string;
+  pax: number;
+  cabin: Cabin;
+}
+
+export interface AirportDTO {
+  code: string;
+  city: string;
+  name?: string;
+  country?: string;
+}
+
+
+export interface FlightResult {
+  id: string;
+  carrier: string;
+  number: string;
+  from: string;
+  to: string;
+  departTime: string;
+  arriveTime: string;
+  durationMinutes: number;
+  cabin: Cabin;
+  price: number;
+}
+
+export interface FlightSearchResponse {
+  items: FlightResult[];
+  total: number;
+}
